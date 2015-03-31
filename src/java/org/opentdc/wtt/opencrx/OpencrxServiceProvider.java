9f1b7e311ae52664bc0b4a821d53d48ead52f2bb
Werner Froidevaux <wfro@users.sourceforge.net>
2015-03-31 19:17:08 +0200

    Clean-up phase 3

--- src/java/org/opentdc/wtt/opencrx/OpencrxServiceProvider.java
+++ src/java/org/opentdc/wtt/opencrx/OpencrxServiceProvider.java
@@ -83,7 +83,10 @@ public class OpencrxServiceProvider implements ServiceProvider {
 	// instance variables
 	protected Logger logger = Logger.getLogger(this.getClass().getName());
 	
-	public OpencrxServiceProvider(ServletContext context) {
+	public OpencrxServiceProvider(
+		ServletContext context,
+		String prefix
+	) {
 		logger.info("> FileImpl()");
 
 		if (url == null) {
