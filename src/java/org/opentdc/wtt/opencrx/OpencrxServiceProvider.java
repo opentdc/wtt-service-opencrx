06a1c45cebe8ce89e4da7a37b3c4dc116edfb427
Werner Froidevaux <wfro@users.sourceforge.net>
2015-03-31 17:49:39 +0200

    Clean-up phase 2

--- src/java/org/opentdc/wtt/opencrx/OpencrxServiceProvider.java
+++ src/java/org/opentdc/wtt/opencrx/OpencrxServiceProvider.java
@@ -60,7 +60,7 @@ import org.opentdc.wtt.ServiceProvider;
 import org.w3c.spi2.Datatypes;
 import org.w3c.spi2.Structures;
 
-public class OpencrxImpl implements ServiceProvider {
+public class OpencrxServiceProvider implements ServiceProvider {
 	
 	public static final String XRI_ACTIVITY_SEGMENT = "xri://@openmdx*org.opencrx.kernel.activity1";
 	public static final String XRI_ACCOUNT_SEGMENT = "xri://@openmdx*org.opencrx.kernel.account1";
@@ -83,7 +83,7 @@ public class OpencrxImpl implements ServiceProvider {
 	// instance variables
 	protected Logger logger = Logger.getLogger(this.getClass().getName());
 	
-	public OpencrxImpl(ServletContext context) {
+	public OpencrxServiceProvider(ServletContext context) {
 		logger.info("> FileImpl()");
 
 		if (url == null) {
